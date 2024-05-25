d397ecd23fd81041611ec837ab0a824cc4dd4cd9
Django Cass <django@dcas.dev>
2024-05-25 18:18:10 +1000

    added nested environment variable expansion fixed environment variable exporting causing duplicate entries

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 build:
 	go run main.go build --config fixtures/v1/pipeline-ubi.yaml --save /tmp/test.tar --v=10
 	docker load < /tmp/test.tar
+	docker inspect image
 
 test:
 	go test ./...
--- pkg/pipelines/env.go
+++ pkg/pipelines/env.go
@@ -6,6 +6,8 @@ import (
 	"github.com/Snakdy/container-build-engine/pkg/pipelines/utils"
 	"github.com/go-logr/logr"
 	"os"
+	"slices"
+	"strings"
 )
 
 type Env struct {
@@ -16,14 +18,26 @@ func (s *Env) Run(ctx *BuildContext) error {
 	log := logr.FromContextOrDiscard(ctx.Context)
 
 	for k, v := range s.options {
-		log.V(5).Info("exporting environment variable", "key", k, "value", v)
-		ctx.ConfigFile.Config.Env = append(ctx.ConfigFile.Config.Env, fmt.Sprintf("%s=%v", k, v))
+		value := os.Expand(v.(string), expandList(ctx.ConfigFile.Config.Env))
+		log.V(5).Info("exporting environment variable", "key", k, "value", v, "expandedValue", value)
+		setOrAppend(ctx.ConfigFile.Config.Env, k, value)
 		if err := os.Setenv(k, v.(string)); err != nil {
-			log.Error(err, "could not export environment variable for usage in later stages", "key", k, "value", v)
+			log.Error(err, "could not export environment variable for usage in later stages", "key", k, "value", v, "expandedValue", value)
 			return err
 		}
 	}
 	return nil
+}
+
+func setOrAppend(vars []string, k, v string) {
+	idx := slices.IndexFunc(vars, func(s string) bool {
+		return strings.HasPrefix(s, k+"=")
+	})
+	if idx < 0 {
+		vars = append(vars, fmt.Sprintf("%s=%v", k, v))
+		return
+	}
+	vars[idx] = fmt.Sprintf("%s=%s", k, v)
 }
 
 func (*Env) Name() string {
